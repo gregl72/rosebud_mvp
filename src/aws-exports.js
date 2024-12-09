7a94cf4ed88a8474c4b85b2cfe4e110eb3f10afb
gregl72 <gregl72@me.com>
2024-12-09 17:09:13 -0600

    auth v2

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,9 +5,9 @@ const awsmobile = {
     "aws_project_region": "us-east-2",
     "Auth": {
         "region": "us-east-2",
-        "userPoolId": "us-east-2_irQwh9CQH",
-        "userPoolWebClientId": "7tg4nkkco2hljjudoem8cv0aql",
-        "mandatorySignIn": false
+        "userPoolId": "us-east-2_tZI9vrJO1",
+        "userPoolWebClientId": "59jjnbmo5g2d34had5tsajqfmt",
+        "mandatorySignIn": true
     }
 };
 
