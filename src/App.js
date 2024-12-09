4675c4bfea5d31bb777c2c0fcae1146aa66ff8f3
gregl72 <gregl72@me.com>
2024-12-09 13:59:50 -0600

    updated auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+// src/App.js
 import React from 'react';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import awsconfig from './aws-exports';
 import Dashboard from './Dashboard';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
-    <Dashboard />
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div>
+          <Dashboard />
+          <button onClick={signOut}>Sign Out</button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
